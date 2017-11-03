816f6b55a807727048138fe3cc370de80a9d148e
ShinChven <shinchven@gmail.com>
2017-11-02 22:24:51 -0500

    Update ubuntu-xenial-amd64.sh

--- ubuntu-xenial-amd64.sh
+++ ubuntu-xenial-amd64.sh
@@ -2,11 +2,11 @@
 
 sudo apt-get remove docker docker-engine docker.io
 sudo apt-get update
-sudo apt-get install \
+sudo apt-get install -y \
     linux-image-extra-$(uname -r) \
     linux-image-extra-virtual
 sudo apt-get update
-sudo apt-get install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -18,4 +18,4 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-get install docker-ce docker-compose
+sudo apt-get install -y docker-ce docker-compose
